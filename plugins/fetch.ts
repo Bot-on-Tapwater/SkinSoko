cf0e3a4d64e2f7baf2c6a8bc33e4c1e77748086d
raymond <stancillousray@gmail.com>
2024-06-18 17:06:39 +0300

    implement func to reduce api calls: plugin to be called before creation of vue app

--- plugins/fetch.ts
+++ plugins/fetch.ts
@@ -1,13 +1,24 @@
+/**contains code to be loaded at the creation of the Nuxtjs app */
 import { useStore } from "~/stores/state";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   const appStore = useStore(nuxtApp.$pinia);
 
+  await appStore.getUser(); /**get user status */
+
+  if (!appStore.cart_loaded) {
+    await appStore.getCartTotalItems();
+  }
 
   if (!appStore.brands_loaded) {
     await appStore.getBrands();
   }
+
   if (!appStore.categories_loaded) {
     await appStore.getCategories();
+  }
+
+  if (!appStore.subcategories_loaded) {
+    await appStore.populateSubcategories();
   }
 });
