e2f0ee1c0aa69782f2b3bf940690573b12dc04ff
raymond <stancillousray@gmail.com>
2024-06-18 17:08:58 +0300

    include some Ts interfaces

--- interfaces/types.ts
+++ interfaces/types.ts
@@ -9,3 +9,23 @@ export interface CategoriesInterface {
     main_category_id: number
     name: string
 }
+
+// export interface Subcategories {
+//     category_name: [
+//         sub_category_id: number,
+//         main_category_id: number,
+//         main_category_name: string,
+//         name: string
+//     ]
+// }
+
+interface MainCategory {
+    sub_category_id: number;
+    main_category_id: number;
+    main_category_name: string;
+    name: string;
+
+}
+export interface Subcategories {
+    [key: string]: MainCategory[]
+}
