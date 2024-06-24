7f85be9993f66842cd48617fb8f681502916fb5c
raymond <stancillousray@gmail.com>
2024-06-24 20:07:37 +0300

    Add top "links"

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -5,6 +5,23 @@
         <CheckoutModal v-if="orderPlacedSuccessfully" />
 
         <div class="checkout-ord-wrapper">
+          <div class="cow-link-dets">
+            <div class="link-wrp">
+              <div class="link-container">
+                <NuxtLink to="/cart">Cart</NuxtLink>
+                <svg width="10px" height="10px" viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000" stroke="#000000" stroke-width="0.01024"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z" fill="#000000"></path></g></svg>
+              </div>
+              <div class="link-container">
+                <NuxtLink to="/checkout">Shipping Address</NuxtLink>
+                <svg width="10px" height="10px" viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000" stroke="#000000" stroke-width="0.01024"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z" fill="#000000"></path></g></svg>
+              </div>
+              <div class="link-container">
+                <NuxtLink to="/">Payment</NuxtLink>
+                <!-- <svg width="10px" height="10px" viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000" stroke="#000000" stroke-width="0.01024"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z" fill="#000000"></path></g></svg> -->
+              </div>
+            </div>
+
+          </div>
           <h1 class="cow-header">Checkout</h1>
           <div class="cow-container">
             <div class="cow-div">
@@ -208,6 +225,24 @@ async function placeOrder() {
   padding: 1rem 1.6rem;
   max-width: 1000px;
   margin: 0 auto;
+
+  .cow-link-dets{
+    padding: 0 .6rem;
+    .link-wrp{
+      display: flex;
+      .link-container{
+        margin: 0 .5rem;
+        display: flex;
+        align-items: center;
+        a{
+          color: var(--webPriColor);
+          padding-right: .3rem;
+          font-size: 1.1rem;
+        }
+      }
+    }
+  }
+
   .cow-header {
     padding: 0 1rem;
     font-weight: 400;
